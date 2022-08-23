a9e5967f6a6fe1e906d6ecc45294370ea78fe174
Hayat Singh <jeet53316@gmail.com>
2022-08-23 11:12:05 +0530

    feat:product added

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
   <br/>
   <br/>
    <div class="flex items-center justify-center">
-    <h1 class="text-3xl text-blue-700 font-bold font-serif text-2xl">
+    <h1 class="text-3xl text-blue-700 font-bold font-serif">
       Nuxt integration with nestjs
     </h1>
   </div>
@@ -69,8 +69,7 @@
           <button
             type="button"
             class="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-          >
-            Button
+          ><NuxtLink to="/product">Button</NuxtLink>
           </button>
         </div>
     </div>
