6fb5a62ef604e84e9be7d2d2454972c544d7833d
satyawan auti <auti.satyavan3@gmail.com>
2022-09-05 16:26:34 +0530

    feat: added category column

--- pages/index.vue
+++ pages/index.vue
@@ -26,7 +26,7 @@
               type="button"
               class="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
             >
-              <NuxtLink to="/book2">Button</NuxtLink>
+              <NuxtLink to="/book3">Button</NuxtLink>
             </button>
           </div>
         </div>
--- pages/product.vue
+++ pages/product.vue
@@ -1,126 +1,181 @@
-<template>
-    <div>
-        <h1 class="font-bold text-center text-2xl mt-10 text-green-400">
-            Product Management
-        </h1>
-        <!-- <div class="grid grid-cols-2"> -->
-        <div class="border-black border-2 m-8 p-8">
-            <!-- @submit="onFormSubmit1()" -->
-            <form @submit.prevent="onFormSubmit1()" class="bg-green-100 border-green-400 rounded-lg border-2 px-12">
-                <table>
-                    <h2 class="text-teal-900 text-xl font-bold pt-6">“Add Product”</h2>
-                    <hr />
-                    <label class="pt-10 py-10" for="ProductName">Product Name:</label><br />
-                    <input v-model="mydata.product.productName" type="text" ref="productName" id="ProductName"
-                        name="ProductName" placeholder="" />
-                    <!-- <span
+<!--  <template>
+  <div>
+    <h1 class="font-bold text-center text-2xl mt-10 text-green-400">
+      Product Management
+    </h1> -->
+<!-- <div class="grid grid-cols-2"> -->
+<!-- <div class="border-black border-2 m-8 p-8"> -->
+<!-- @submit="onFormSubmit1()" -->
+<!-- <form
+        @submit.prevent="onFormSubmit1()"
+        class="bg-green-100 border-green-400 rounded-lg border-2 px-12"
+      >
+        <table>
+          <h2 class="text-teal-900 text-xl font-bold pt-6">“Add Product”</h2>
+          <hr />
+          <label class="pt-10 py-10" for="ProductName">Product Name:</label
+          ><br />
+          <input
+            v-model="mydata.product.productName"
+            type="text"
+            ref="productName"
+            id="ProductName"
+            name="ProductName"
+            placeholder=""
+          />
+           <span
             v-for="error in v$.productName.$errors"
             :key="error.$uid"
             class="text-red-700"
             >{{ error.$messages }}</span
           > -->
-                    <span v-for="error in v$.productName.$errors" :key="error.$uid" class="text-red-600">{{
-                            error.$message
-                    }}
-                    </span>
-                    <br /><br />
-                    <label for="Price">Price:</label><br />
-                    <input v-model="mydata.product.price" type="number" ref="price" id="Price" name="Price"
-                        placeholder="" />
-                    <span v-for="error in v$.price.$errors" :key="error.$uid" class="text-red-600">{{ error.$message }}
-                    </span><br /><br />
-                    <label for="Stock">Stock:</label>
-                    <select v-model="mydata.product.stock" class="p-1" name="Stock" id="Stock" ref="stock">
-                        <option value="Available" selected>Available</option>
-                        <option value="OutOfStock">Out of Stock</option>
-                    </select>
-                    <span v-for="error in v$.stock.$errors" :key="error.$uid" class="text-red-600">{{ error.$message }}
-                    </span><br /><br />
-                    <label for="Size">Size:</label>
-                    <select v-model="mydata.product.size" class="p-1" name="Size" id="Size" ref="size">
-                        <!-- <option value="XS">XS</option> -->
-                        <option value="S">S</option>
-                        <option value="M" selected>M</option>
-                        <option value="L">L</option>
-                        <option value="XL">XL</option>
-                    </select>
-                    <span v-for="error in v$.size.$errors" :key="error.$uid" class="text-red-600">{{ error.$message }}
-                    </span><br /><br />
-                    <label for="Image">Upload Image</label><br /><br />
-                    <input type="file" id="Image" name="Image" /><br /><br />
-                    <div class="mt-10">
-                        <button
-                            class="py-1 px-5 mr-5 bg-black hover:bg-blue-400 text-white font-bold text-center rounded-md mb-3"
-                            type="submit">
-                            Add Product
-                        </button>
-                        <!-- \@click="onFormSubmit1()" -->
-                        <button
-                            class="py-1 px-5 bg-black hover:bg-blue-400 text-white font-bold text-center rounded-md mb-3"
-                            type="reset">
-                            Reset
-                        </button>
-                    </div>
-                </table>
-                <span v-for="error in v$.$errors" :key="error.$uid">{{ error.$property }}----{{ error.$message }}</span>
-            </form>
-        </div>
-        <div class="border-black border-2 m-8 p-8">
-            <table class="list">
-                <tr>
-                    <!-- <th class="px-4 border-black rounded-lg border-2">id</th> -->
-                    <th class="px-4 border-green-400 rounded-lg border-4">ProductId</th>
-                    <th class="px-4 border-green-400 rounded-lg border-4">ProductName</th>
-                    <th class="px-4 border-green-400 rounded-lg border-4">Price</th>
-                    <th class="px-4 border-green-400 rounded-lg border-4">Stock</th>
-                    <th class="px-4 border-green-400 rounded-lg border-4">Size</th>
-                    <th class="px-4 border-green-400 rounded-lg border-4">Image</th>
-                    <th class="px-4 border-green-400 rounded-lg border-4">Action</th>
-                </tr>
-                <tr v-for="item in mydata.allProduct" :key="item.id">
-                    <!-- <td class="px-4 border-black rounded-lg border-2">{{item.id=i+1}}</td> -->
-                    <td class="px-4 border-green-400 rounded-lg border-4">
-                        {{ item.id }}
-                    </td>
-                    <td class="px-4 border-green-400 rounded-lg border-4">
-                        {{ item.productName }}
-                    </td>
-                    <td class="px-4 border-green-400 rounded-lg border-4">
-                        {{ item.price }}
-                    </td>
-                    <td class="px-4 border-green-400 rounded-lg border-4">
-                        {{ item.stock }}
-                    </td>
-                    <td class="px-4 border-green-400 rounded-lg border-4">
-                        {{ item.size }}
-                    </td>
-                    <td class="px-4 border-green-400 rounded-lg border-4">
-                        <img src="{{item.image}}" alt="" srcset="" />
-                    </td>
-                    <td class="px-4 border-green-400 rounded-lg border-4">
-                        {{ item.Action }}
-                        <button class="mx-3 rounded-lg bg-red-600 hover:bg-red-700 text-white w-20"
-                            @click="onDeleteOfProduct(item.id)">
-                            Delete
-                        </button>
-                        <button type="submit" class="mx-3 rounded-lg bg-green-600 hover:bg-green-600 text-white w-20"
-                            @click="editProduct(item.id)">
-                            Edit
-                        </button>
-                    </td>
-                </tr>
-            </table>
-        </div>
-        <p>
-            {{ mydata.product.statusCode }}
-            <!-- </div> -->
-            <!-- <p>{{ allProduct }}</p> -->
-        </p>
-        <!-- <p>{{ message }}</p> -->
-    </div>
-</template>
-<script setup lang="ts">
-import useVuelidate from "@vuelidate/core";
+<!-- <span
+            v-for="error in v$.productName.$errors"
+            :key="error.$uid"
+            class="text-red-600"
+            >{{ error.$message }}
+          </span>
+          <br /><br />
+          <label for="Price">Price:</label
+          ><br />
+<input
+            v-model="mydata.product.price"
+            type="number"
+            ref="price"
+            id="Price"
+            name="Price"
+            placeholder=""
+          />
+          <span
+            v-for="error in v$.price.$errors"
+            :key="error.$uid"
+            class="text-red-600"
+            >{{ error.$message }} </span
+          ><br /><br />
+          <label for="Stock">Stock:</label>
+          <select
+            v-model="mydata.product.stock"
+            class="p-1"
+            name="Stock"
+            id="Stock"
+            ref="stock"
+          >
+<option value="Available" selected>Available</option>
+            <option value="OutOfStock">Out of Stock</option>
+          </select>
+          <span
+            v-for="error in v$.stock.$errors"
+            :key="error.$uid"
+            class="text-red-600"
+            >{{ error.$message }} </span
+          ><br /><br />
+          <label for="Size">Size:</label>
+          <select
+            v-model="mydata.product.size"
+            class="p-1"
+            name="Size"
+            id="Size"
+            ref="size"
+          > -->
+<!-- 
+<option value="XS">XS</option>
+<option value="S">S</option>
+            <option value="M" selected>M</option>
+            <option value="L">L</option>
+            <option value="XL">XL</option>
+          </select>
+          <span
+            v-for="error in v$.size.$errors"
+            :key="error.$uid"
+            class="text-red-600"
+            >{{ error.$message }} </span
+          ><br /><br />
+          <label for="Image">Upload Image</label
+          ><br /><br />
+          <input type="file" id="Image" name="Image" /><br /><br />
+          <div class="mt-10">
+            <button
+              class="py-1 px-5 mr-5 bg-black hover:bg-blue-400 text-white font-bold text-center rounded-md mb-3"
+              type="submit"
+            >
+              Add Product
+            </button>
+           
+<button
+              class="py-1 px-5 bg-black hover:bg-blue-400 text-white font-bold text-center rounded-md mb-3"
+              type="reset"
+            >
+              Reset
+            </button>
+          </div>
+        </table>
+<span v-for="error in v$.$errors" :key="error.$uid"
+          >{{ error.$property }}----{{ error.$message }}</span
+        >
+      </form>
+</div>
+    <div class="border-black border-2 m-8 p-8">
+      <table class="list">
+<tr>
+           <th class="px-4 border-black rounded-lg border-2">id</th> 
+          <th class="px-4 border-green-400 rounded-lg border-4">ProductId</th>
+          <th class="px-4 border-green-400 rounded-lg border-4">ProductName</th>
+          <th class="px-4 border-green-400 rounded-lg border-4">Price</th>
+          <th class="px-4 border-green-400 rounded-lg border-4">Stock</th>
+          <th class="px-4 border-green-400 rounded-lg border-4">Size</th>
+          <th class="px-4 border-green-400 rounded-lg border-4">Image</th>
+          <th class="px-4 border-green-400 rounded-lg border-4">Action</th>
+        </tr>
+<tr v-for="item in mydata.allProduct" :key="item.id">
+           <td class="px-4 border-black rounded-lg border-2">{{item.id=i+1}}</td>
+          <td class="px-4 border-green-400 rounded-lg border-4">
+            {{ item.id }}
+          </td>
+          <td class="px-4 border-green-400 rounded-lg border-4">
+            {{ item.productName }}
+          </td>
+          <td class="px-4 border-green-400 rounded-lg border-4">
+            {{ item.price }}
+          </td>
+          <td class="px-4 border-green-400 rounded-lg border-4">
+            {{ item.stock }}
+          </td>
+          <td class="px-4 border-green-400 rounded-lg border-4">
+            {{ item.size }}
+          </td>
+          <td class="px-4 border-green-400 rounded-lg border-4">
+            <img src="{{item.image}}" alt="" srcset="" />
+          </td>
+<td class="px-4 border-green-400 rounded-lg border-4">
+            {{ item.Action }}
+            <button
+              class="mx-3 rounded-lg bg-red-600 hover:bg-red-700 text-white w-20"
+              @click="onDeleteOfProduct(item.id)"
+            >
+              Delete
+            </button>
+            <button
+              type="submit"
+              class="mx-3 rounded-lg bg-green-600 hover:bg-green-600 text-white w-20"
+              @click="editProduct(item.id)"
+            >
+              Edit
+            </button>
+          </td>
+        </tr>
+      </table> 
+</div>
+    <p>
+      {{ mydata.product.statusCode }}
+       </div> 
+       <p>{{ allProduct }}</p> 
+    </p>
+     <p>{{ message }}</p> 
+</div>
+</template> -->
+
+<!-- <script setup lang="ts"> -->
+<!-- import useVuelidate from "@vuelidate/core";
 import { maxLength, minLength, required } from "@vuelidate/validators";
 
 const mydata = reactive({
@@ -148,7 +203,7 @@ const rules = computed(() => {
 const v$ = useVuelidate(rules, mydata.product);
 
 getProductAPI();
-GET API
+// GET API
 async function getProductAPI() {
     mydata.allProduct = await $fetch("http://localhost:3001/product/allData");
 }
@@ -239,5 +294,5 @@ async function onDeleteOfProduct(id) {
         method: "DELETE",
     });
     getProductAPI();
-}
-</script>
+} -->
+<!-- </script> -->
